760aa9212e056350aefd255383fb6638682fd77e
Raphaël Mongeau <raphaelsimeon@gmail.com>
2014-12-29 10:32:19 -0500

    comments

--- Main.hs
+++ Main.hs
@@ -5,6 +5,9 @@ module Main where
 import Data.Typeable
 
 class (Typeable a) => UnionBoolAndList a where
+    -- Is it possible to have a class without a body?
+    -- I am using class as a way to define a constrain on the type of an argument, not
+    -- to have member functions.
     id :: a -> a
 
 instance UnionBoolAndList ([Bool]) where
@@ -14,6 +17,7 @@ instance UnionBoolAndList Bool where
     id x = x
 
 f :: (UnionBoolAndList a) => a -> String
+-- warning, the type inference is deciding what is the type of "cast".
 f (cast -> Just x) = showBoolList x
 f (cast -> Just x) = showBool x
 
